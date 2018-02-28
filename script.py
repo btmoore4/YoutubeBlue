2a23000ba8fb7d3f951b07f55c34d20baa94ac05
Ben Moore <benmoore@bens-mbp.home>
2018-02-27 22:32:21 -0500

    Added a Cleanup Job after 3 Days

--- script.py
+++ script.py
@@ -49,12 +49,12 @@ def youtube_blue_main():
         for line in inputfile:
             KEY = line
 
-    repeat_task(DOWNLOAD_INTERVAL, CHECK_INTERVAL, KEY, OUTPUT_DIR)
+    repeat_task(DOWNLOAD_INTERVAL, CHECK_INTERVAL, KEY, OUTPUT_DIR, 2)
 
 
 #Downloads Videos from Channels in Channel List
-def get_videos(channel_list, key, output_directory):
-    TIME_STRING = (datetime.datetime.now() - datetime.timedelta(days=3)).isoformat()
+def get_videos(channel_list, key, output_directory, num_days):
+    TIME_STRING = (datetime.datetime.now() - datetime.timedelta(days=num_days)).isoformat()
     TIME_STRING = TIME_STRING.split('T')[0]+"T00:00:00Z"
     youtube = build('youtube', 'v3', developerKey=key)
     VIDEO_LIST = []
@@ -89,19 +89,26 @@ def get_videos(channel_list, key, output_directory):
     for VIDEO in VIDEO_LIST: 
         call(["youtube-dl", "-o", output_directory+"/%(title)s.%(ext)s", VIDEO])
 
+    delete_old(output_directory, num_days+1)
+
 
 #Executes get_videos Periodically 
-def repeat_task(execution_interval, check_interval, key, output_directory):
+def repeat_task(execution_interval, check_interval, key, output_directory, num_days):
     while (True):
         start_time = datetime.datetime.now()
         execute_time = start_time + datetime.timedelta(seconds=execution_interval)
         print("Starting Process at " + start_time.isoformat())
         print("Next Execution at " + execute_time.isoformat())
-        get_videos(CHANNEL_LIST, key, output_directory)
+        get_videos(CHANNEL_LIST, key, output_directory, num_days)
         print("Execution Complete waiting until " + execute_time.isoformat())
         while(execute_time > datetime.datetime.now()):
             time.sleep(check_interval)
 
+#Deletes older videos
+def delete_old(output_directory, days_delete):
+    print("Deleting the Following Videos...")
+    call(["find", output_directory, "-mindepth", "1", "-mtime", "+"+str(days_delete), "-depth", "-print"])
+    call(["find", output_directory, "-mindepth", "1", "-mtime", "+"+str(days_delete), "-delete"])
 
 if __name__ == "__main__":
     youtube_blue_main()
